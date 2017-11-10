0b2a87bc602c7d4a60c2c6d0c84e3fe1465be89e
Erich <Erich@ERICH>
2017-11-10 15:59:11 +0900

    Checker_board_detection

--- 3D_Face_scanning.vc140/check_Board_detection/main.cpp
+++ 3D_Face_scanning.vc140/check_Board_detection/main.cpp
@@ -35,120 +35,113 @@ int main()
 	vector <Point2f> out_centers;
 	//vector <Point2f> centers;
 
-	const int GridCol = 11;
-	const int GirdRow = 8;
+	const int GridCol = 10;
+	const int GirdRow = 7;
 	
-	while (1) 
-	{
-		const uint8_t * color_image = (const uint8_t *)dev->get_frame_data(rs::stream::color);
 
-		for(int i=0;i<30;i++)
+	const uint8_t * color_image = (const uint8_t *)dev->get_frame_data(rs::stream::color);
+
+	for (int i = 0; i<30; i++)
 		dev->wait_for_frames();
-		color_image = (const uint8_t *)dev->get_frame_data(rs::stream::color);
-		
-		Mat image(Size(640, 480), CV_8UC3, (void*)color_image, Mat::AUTO_STEP);
+	color_image = (const uint8_t *)dev->get_frame_data(rs::stream::color);
+
+	Mat image(Size(640, 480), CV_8UC3, (void*)color_image, Mat::AUTO_STEP);
 
 
-		//cvtColor(image, image, CV_RGB2GRAY);
-		//image=imread("cow.jpg");
-		std::cout << "Start Finding\n";
+	//cvtColor(image, image, CV_RGB2GRAY);
+	//image=imread("cow.jpg");
+	std::cout << "Start Finding\n";
 
-		bool found = false;
-		for (int i = GirdRow; i >= 3; i--)
+	int col, row;
+	bool found = false;
+	for (int i = GirdRow; i >= 3; i--)
+	{
+		found = false; int j;
+		for (j = GridCol; j >= 3; j--)
 		{
-			found = false;
-			for (int j = GridCol; j >= 3; j--)
-			{
-				Size ss(j, i); vector <Point2f> out_centers;
-				outimage = image;
+			Size ss(j, i);
+			outimage = image;
 
-				std::cout << "Start Finding 가로 세로" << j << " " << i << "\n";
-				found = findChessboardCorners(outimage, ss, out_centers);
-
-				if (found > 0)
-				{
-					drawChessboardCorners(outimage, ss, out_centers, found);
-					cout << "find: " << found << std::endl;
-
-					for(int i=0;i<out_centers.size();i++)
-						std::cout << out_centers[i].x << " " << out_centers[i].y << "\n";
-					break;
-				}
-
-				else cout << "find: " << found << std::endl;
-
-			}
+			std::cout << "Start Finding 가로 세로" << j << " " << i << "\n";
+			found = findChessboardCorners(outimage, ss, out_centers);
 
 			if (found > 0)
 			{
-				std::cout << "Hi";
+				drawChessboardCorners(outimage, ss, out_centers, found);
+				cout << "find: " << found << std::endl;
+
+				for (int i = 0; i<out_centers.size(); i++)
+					std::cout << out_centers[i].x << " " << out_centers[i].y << "\n";
 				break;
 			}
-		}
 
+			else cout << "find: " << found << std::endl;
+
+		}
 
 		if (found > 0)
 		{
-			outimage = image;
+			//std::cout << "Hi";
+			col = j;
+			row = i;
 			break;
 		}
-
-		outimage = image;
-		break;
 	}
+
+
+	if (found > 0)outimage = image;
+	
+
+	outimage = image;
+	
 	//display the result
+	for (int i = 0; i<out_centers.size(); i++)
+		std::cout << out_centers[i].x << " " << out_centers[i].y << "\n";
+
+	float sumOfdis = 0.0f;
+	float average = 0.0f;
+	std::vector<float> dis;
+
+	std::cout << "#Detected Point is " << out_centers.size() << "\n";
+	
+	for (int i = 0; i < out_centers.size()-1; i++)
+	{
+		if (i == col-1)continue;
+
+		float _distance;
+		float x1, x2;
+		float y1, y2;
+
+		x1 = out_centers[i].x;
+		y1 = out_centers[i].y;
+		
+		x2 = out_centers[i+1].x;
+		y2 = out_centers[i+1].y;
+		
+		_distance = sqrt((x1 - x2)*(x1 - x2) + (y2 - y1)*(y2 - y1));
+
+		std::cout << _distance << "\n";
+		sumOfdis += _distance;
+		dis.push_back(_distance);
+		
+	} 
+	//average /= sumOfdis;
+	////std::cout << "Average of distance is "<<average;
+	////std::cout << "average of (square Deviation) is " ;
+
+	//float stdv = 0.0f;
+
+	//for (int i = 0; i < dis.size(); i++)
+	//{
+	//	stdv+=(dis[i] - average);
+	//}
+
+	//stdv /= (out_centers.size() - 1);
+	//std::cout << stdv << "\n";
+
 	cv::namedWindow("Display window", WINDOW_AUTOSIZE);
 	cv::imshow("Display window", outimage);
-
 	cv::waitKey(0);
+
 	return 0;
 }
-
-//
-//int main()
-//{
-//	Size size(5,5);
-//	Mat outimage;
-//	
-//	//vector <Point2f> centers;
-//	
-//	//cvtColor(outimage, outimage, CV_RGB2GRAY);
-//	
-//	for (int i=6;i>=3;i--) 
-//	{
-//		bool found = false;
-//		bool flag = false;
-//		for (int j=6;j>=3;j--)
-//		{
-//			if (j == 5 && i == 5)std::cout << "Nellow";
-//			Size ss(j, i); vector <Point2f> out_centers;
-//			outimage = imread("cow.jpg");
-//			
-//			std::cout << "Start Finding 가로 세로"<<j<<" "<<i<<"\n";
-//			bool found = findChessboardCorners(outimage, ss, out_centers);
-//			
-//			if (found > 0)
-//			{
-//				drawChessboardCorners(outimage, ss, out_centers, found);
-//				cout << "bfind: " << found << std::endl;
-//				flag = true;
-//				break;
-//			}
-//
-//			else cout << "find: " << found<<std::endl;
-//			
-//		}
-//
-//		if (flag) 
-//		{
-//			std::cout << "Hi";
-//			break; 
-//		}
-//	}
-//
-//	cv::namedWindow("Display window", WINDOW_AUTOSIZE);
-//	cv::imshow("Display window", outimage);
-//
-//	cv::waitKey(0);
-//	return 0;
-//}
