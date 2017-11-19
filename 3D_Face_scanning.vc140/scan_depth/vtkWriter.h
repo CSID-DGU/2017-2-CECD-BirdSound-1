7fc6ab5c7840bbbc8f7052139860a0d0701a2399
JH <JH@DESKTOP-TRRC6F8>
2017-11-19 15:23:57 +0900

    vtkWriter - quality upgrade

--- 3D_Face_scanning.vc140/scan_depth/vtkWriter.h
+++ 3D_Face_scanning.vc140/scan_depth/vtkWriter.h
@@ -27,30 +27,50 @@ public:
 		subPOINTS = subWidth*subHeight;
 		saveVTK();
 	}
+	double abs(double A) {
+		if (A > 0) {
+			return A;
+		}
+		else {
+			return -A;
+		}
+	}
 	void saveVTK() {
 		int ct = 0;
 		std::ofstream fout(filename);
 		fout << "# vtk DataFile Version 3.0\nvtk output\nASCII\nDATASET POLYDATA\nPOINTS " << subPOINTS << " float" << std::endl;
 		for (int i = 0; i < subPOINTS; i++) {
 			fout << Cloud->points[i].x << " " << Cloud->points[i].y << " " << Cloud->points[i].z << std::endl;
-			if ((i + subWidth < subPOINTS) && ((i + 1) % subWidth != 0)) {
-				if ((Cloud->points[i].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + 1].x != 0))
-					ct += 1;
-			}
-			if (((i + subWidth + 1) < subPOINTS) && ((i + 1) % subWidth != 0)) {
-				if ((Cloud->points[i + 1].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + subWidth + 1].x != 0))
-					ct += 1;
+			if ((i + subWidth + 1 < subPOINTS) && ((i + 1) % subWidth != 0)) {
+				if (abs(Cloud->points[i].z - Cloud->points[i + subWidth + 1].z) > abs(Cloud->points[i + 1].z - Cloud->points[i + subWidth].z)) {
+					if ((Cloud->points[i].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + 1].x != 0))
+						ct += 1;
+					if ((Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + 1].x != 0) && (Cloud->points[i + subWidth + 1].x != 0))
+						ct += 1;
+				}
+				else {
+					if ((Cloud->points[i].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i +subWidth+ 1].x != 0))
+						ct += 1;
+					if ((Cloud->points[i + 1].x != 0) && (Cloud->points[i + subWidth+1].x != 0) && (Cloud->points[i].x != 0))
+						ct += 1;
+				}
 			}
 		}
 		fout << "\nPOLYGONS " << ct << " " << ct * 4 << std::endl;
 		for (int i = 0; i < subPOINTS; i++) {
-			if ((i + subWidth < subPOINTS) && ((i + 1) % subWidth != 0)) {
-				if ((Cloud->points[i].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + 1].x != 0))
-					fout << "3 " << i << " " << i + subWidth << " " << i + 1 << std::endl;
-			}
-			if (((i + subWidth + 1) < subPOINTS) && ((i + 1) % subWidth != 0)) {
-				if ((Cloud->points[i + 1].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + subWidth + 1].x != 0))
-					fout << "3 " << i + subWidth << " " << i + 1 << " " << i + subWidth + 1 << std::endl;
+			if ((i + subWidth + 1 < subPOINTS) && ((i + 1) % subWidth != 0)) {
+				if (abs(Cloud->points[i].z - Cloud->points[i + subWidth + 1].z) > abs(Cloud->points[i + 1].z - Cloud->points[i + subWidth].z)) {
+					if ((Cloud->points[i].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + 1].x != 0))
+						fout << "3 " << i << " " << i + subWidth << " " << i + 1 << std::endl;
+					if ((Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + 1].x != 0) && (Cloud->points[i + subWidth + 1].x != 0))
+						fout << "3 " << i + subWidth << " " << i + 1 << " " << i + subWidth + 1 << std::endl;
+				}
+				else {
+					if ((Cloud->points[i].x != 0) && (Cloud->points[i + subWidth].x != 0) && (Cloud->points[i + subWidth + 1].x != 0))
+						fout << "3 " << i << " " << i + subWidth << " " << i + subWidth + 1 << std::endl;
+					if ((Cloud->points[i + 1].x != 0) && (Cloud->points[i + subWidth + 1].x != 0) && (Cloud->points[i].x != 0))
+						fout << "3 " << i + 1 << " " << i + subWidth + 1 << " " << i << std::endl;
+				}
 			}
 		}
 		fout.close();
