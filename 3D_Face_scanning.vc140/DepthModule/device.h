e29def0ea79a1fe80dc98eccffd64002b67cd339
장혁 권 <nadaadhj@gmail.com>
2018-01-29 09:33:58 +0900

    RealSense with vtk

--- 3D_Face_scanning.vc140/DepthModule/device.h
+++ 3D_Face_scanning.vc140/DepthModule/device.h
@@ -1,18 +1,32 @@
-#pragma once
 #ifndef DEVICE_H
 #define DEVICE_H
 #define MAX_CAM_NUM 3
 #define MIN_CAM_NUM 0
 //#define STB_IMAGE_WRITE_IMPLEMENTATION
 //#include "stb_image_write.h"
+
 #include "common.h"
 #include "libRealsense2\rs.hpp"
-#include "vtk-8.1\vtkPoints.h"
 #include <iostream>
 #include <iomanip>
 #include <cstdint>
 #include <sstream>
 #include <map>
+#include<vtkPoints.h>
+#include"vtkRenderWindow.h"
+#include"vtkRenderer.h"
+#include<vtkCellArray.h>
+#include<vtkPolyData.h>
+#include<vtkPolyDataMapper.h>
+
+#include <vtkCamera.h>
+#include<vtkRenderer.h>
+#include<vtkRenderWindow.h>
+#include<vtkRenderWindowInteractor.h>
+#include<vtkInteractorStyle.h>
+#include"vtkSphereSource.h"
+#include"vtkSTLWriter.h"
+#include"vtkOBJExporter.h"
 
 enum { color, depth };
 
@@ -40,11 +54,17 @@ public:
 	std::vector<rs2::frame> Realsense::capture(int devNum, int streamType, int frameNum, int restNum = 10);
 	std::string Realsense::saveImage(rs2::frame &frame, string filepath, int filetype);
 	vtkPoints* Realsense::frameToVtkPoints(rs2::frame &frame);
-
+	/*1이면 stl 2면 obj로 저장*/
+	void MeshConstruct(vtkPoints *points, int saveType = 3);
 private:
 	bool isInit();
-	void Realsense::restFrame(unit &cam_unit, int num);
+	void restFrame(unit &cam_unit, int num);
 
+private:
+	const double INF = 9999999999999;
+	const int height = 720;
+	const int width = 1280;
+	double getDistane(double *src, double *tar);
 };
 
 #endif
